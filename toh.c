7ba6145eb8bb26de2be671e536b7bb958e244c13
GSP-YT <128293212+GSP-YT@users.noreply.github.com>
2023-06-04 10:18:33 +0530

    Update toh.c

--- toh.c
+++ toh.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
-void tower(int n, char BEG, char AUX, char END){
+int tower(int n, char BEG, char AUX, char END,int count){
     if(n==1){
-        printf("Move Disk- %d From %c To %c\n",n,BEG,END);
-        return;
+        printf("Step %d : Move Disk- %d From %c To %c\n",count++,n,BEG,END);
+        return count;
     }
-    tower(n-1,BEG,END,AUX);
-    printf("Move Disk- %d From %c To %c\n",n,BEG,END);
-    tower(n-1,AUX,BEG,END);
+    count=tower(n-1,BEG,END,AUX,count);
+    printf("Step %d : Move Disk- %d From %c To %c\n",count++,n,BEG,END);
+    count=tower(n-1,AUX,BEG,END,count);
+    return count;
 }
 void main(){
-    int n;
+    int n,count=1;
     printf("Enter Number Of Disks : ");
     scanf("%d", &n);
-    tower(n,'A','B','C');
+    count=tower(n,'A','B','C',count);
+    printf("Total No. Of Steps Is %d",count);
 }
